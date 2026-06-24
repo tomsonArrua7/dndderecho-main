0d43adc0cf0b6e33e182fb7a27b2623da86ef3a7
Tomas Arrua <tgonzalezarrua@gmail.com>
2026-06-24 13:05:18 -0300

    fix: remove AnimatePresence mode=wait to prevent routing hangs

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -17,18 +17,15 @@ export const Layout = () => {
       {!isFocusMode && <Navbar />}
       
       <main className="flex-1 flex flex-col relative">
-        <AnimatePresence mode="wait">
-          <motion.div
-            key={location.pathname}
-            initial={{ opacity: 0, y: 10 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -10 }}
-            transition={{ duration: 0.3, ease: [0.22, 1, 0.36, 1] }}
-            className="flex-1 flex flex-col"
-          >
-            <Outlet />
-          </motion.div>
-        </AnimatePresence>
+        <motion.div
+          key={location.pathname}
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.3, ease: [0.22, 1, 0.36, 1] }}
+          className="flex-1 flex flex-col"
+        >
+          <Outlet />
+        </motion.div>
       </main>
 
       {!isFocusMode && <Footer />}
